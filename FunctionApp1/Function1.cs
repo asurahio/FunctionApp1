379ad37b9cc8b30c9660015a580caba0ce463d83
Abdul Bassit Surahio <asurahio@innovative-pk.com>
2023-06-08 14:46:59 +0500

    4th commit

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -26,5 +26,10 @@ namespace FunctionApp1
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
-    };
+    }
 }
+{
+            return name != null
+                ? (ActionResult) new OkObjectResult($"Hello, {name}")
+                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
